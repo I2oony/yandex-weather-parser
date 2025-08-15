9ae6de30a30df070d8265fafafe1869fcdd66575
Polina Palamar <ppalamar28@gmail.com>
2025-08-15 15:57:11 +0300

    migrated to parametric weather getter; added collecting forecast to dict

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import json
+
 import parse_with_selenium
 
 if __name__ == "__main__":
@@ -5,4 +7,6 @@ if __name__ == "__main__":
     # TODO: should be generated depending on input city later on
     url = "https://yandex.ru/pogoda/ru?lat=59.938784&lon=30.314997"
 
-    parse_with_selenium.parse(url)
+    forecast = parse_with_selenium.parse(url)
+
+    print(json.dumps(forecast, indent=4, ensure_ascii=False))
--- app/parse_with_selenium.py
+++ app/parse_with_selenium.py
@@ -1,52 +1,49 @@
+from enum import Enum
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 def parse(url):
     options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
+    options.add_argument("headless")
+    # used to make the window wide, since with the narrow one weather desc is hidden and can't be parsed
+    options.add_argument("--window-size=1920,1024")
     driver = webdriver.Chrome(options=options)
-    # driver.implicitly_wait(10)
+
+    forecast = []
 
     driver.get(url)
     try:
-        forecast = driver.find_element(By.CSS_SELECTOR, "article[class^='MainPage_appForecast']")
-        day_cards = forecast.find_elements(By.TAG_NAME, "a")
+        forecast_element = driver.find_element(By.CSS_SELECTOR, "article[class^='MainPage_appForecast']")
+        day_cards = forecast_element.find_elements(By.TAG_NAME, "a")
 
         for day_card in day_cards:
-            day = day_card.find_element(By.XPATH, "article/h3[contains(@style, 'day-title')]")
+            day_block = {
+                "date": day_card.find_element(By.XPATH, "article/h3[contains(@style, 'day-title')]").text
+            }
+
+            for time_of_day in TimeOfDay:
+                day_block[time_of_day.value] = {
+                    "temp": day_card.find_element(By.XPATH, f"article/div[contains(@style, '{time_of_day.value}-temp')]").text,
+                    "press": day_card.find_element(By.XPATH, f"article/div[contains(@style, '{time_of_day.value}-press')]").text,
+                    "hum": day_card.find_element(By.XPATH, f"article/div[contains(@style, '{time_of_day.value}-hum')]").text,
+                    "weather": day_card.find_element(By.XPATH, f"article/div[contains(@style, '{time_of_day.value}-text')]").text,
+                }
             
-            # morning
-            m_temp = day_card.find_element(By.XPATH, "article/div[contains(@style, 'm-temp')]")
-            m_press = day_card.find_element(By.XPATH, "article/div[contains(@style, 'm-press')]")
-            m_hum = day_card.find_element(By.XPATH, "article/div[contains(@style, 'm-hum')]")
-
-            # FIXME: won't work - no text displayed ???
-            weather_type = day_card.find_element(By.XPATH, "article/div[contains(@style, 'text')]")
-            print(f"weather_type={str(weather_type)}")
-
-            # day
-            d_temp = day_card.find_element(By.XPATH, "article/div[contains(@style, 'd-temp')]")
-            d_press = day_card.find_element(By.XPATH, "article/div[contains(@style, 'd-press')]")
-            d_hum = day_card.find_element(By.XPATH, "article/div[contains(@style, 'd-hum')]")
-
-            # evening
-            e_temp = day_card.find_element(By.XPATH, "article/div[contains(@style, 'e-temp')]")
-            e_press = day_card.find_element(By.XPATH, "article/div[contains(@style, 'e-press')]")
-            e_hum = day_card.find_element(By.XPATH, "article/div[contains(@style, 'e-hum')]")
-
-            # nigth
-            n_temp = day_card.find_element(By.XPATH, "article/div[contains(@style, 'n-temp')]")
-            n_press = day_card.find_element(By.XPATH, "article/div[contains(@style, 'n-press')]")
-            n_hum = day_card.find_element(By.XPATH, "article/div[contains(@style, 'n-hum')]")
-
-            print(day.text)
-            print(f"{m_temp.text} -- {m_press.text} -- {m_hum.text} -- {weather_type.text}")
-            print(f"{d_temp.text} -- {d_press.text} -- {d_hum.text} -- {weather_type.text}")
-            print(f"{e_temp.text} -- {e_press.text} -- {e_hum.text} -- {weather_type.text}")
-            print(f"{n_temp.text} -- {n_press.text} -- {n_hum.text} -- {weather_type.text}")
+            forecast.append(day_block)
     except NoSuchElementException as e:
-        print(f"Some element not found: {e.msg}")
+        print(f"Some element was not found: {e.msg}")
+
+    driver.quit()
+    return forecast
 
 
-    # driver.quit()
+class TimeOfDay(Enum):
+    MORNING = "m"
+    DAY = "d"
+    EVENING = "e"
+    NIGHT = "n"
+
+    def __iter__(self):
+        return iter(list(self.MORNING, self.DAY, self.EVENING, self.NIGHT))
